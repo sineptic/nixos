05fb9afedd9ddfed35b1beaf49bdd1cedb01f5c0
sineptic <sineptic0@gmail.com>
2025-02-18 19:31:09 +0300

    update to unstable pkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # NixOS official package source, using the nixos-24.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = {
